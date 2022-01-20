cd47d8c60d336b34a4e51fc49c64c0b71f3854b7
SE404 <sergey.f.emelyanov@gmail.com>
2022-01-20 18:53:19 +0300

    amended webpack-config

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route exact path="/" element={<Main />} />
         <Route path="/verify_email" element={<VerifyEmail />} />
         <Route
           path="/continue_registration"
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,45 @@
-const path = require('path');
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: ["regenerator-runtime/runtime.js", "./src/index.js"] ,
-    output: {
-        path: path.join(__dirname, "/dist"),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.(png|jpg)$/,
-                loader: 'url-loader'
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg|ttf)$/i,
-                use: [
-                    {
-                        loader: "file-loader"
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
-    ]
-}
+  entry: ["regenerator-runtime/runtime.js", "./src/index.js"],
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: "url-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|ttf)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
