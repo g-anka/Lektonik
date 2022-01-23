744bc4ab4e52d52027b20e917105836e9ab92508
Anastasia Kalashnikova <g.ankalashik@gmail.com>
2022-01-23 06:10:15 +0300

    Изменения в стилях, классах и инпутах
    + прописаны основные стили в style.css
    + Формы переписаны с использованием react-hook-form
    + Сделана валидация
    
    - Проблема с Cors не решена
    - Осталась работа с классами в продолжении регистрации и начале заполнения формы пользователя

--- src/components/Authorization.js
+++ src/components/Authorization.js
@@ -1,5 +1,6 @@
-import React, {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import "../styles/Authorization.css";
 import "../styles/style.css";
 import eyeOpen from "../img/eyeOpen.svg";
@@ -8,36 +9,34 @@ import 'regenerator-runtime/runtime';
 
 function Authorization() {
 
-    const navigate = useNavigate(); //Для перехода
+//Для перехода
+    const navigate = useNavigate();
 
-    //ВХОД
-    //изменение значений в инпутах
-    const [signInValue, setSignInValue] = useState({
-        email: "",
-        password: ""
+//формы реализованы с помощью react-hook-form
+    const {
+        register,
+        formState: {
+            errors
+        },
+        handleSubmit,
+        getValues,
+    } = useForm({
+        mode: "onTouched"
     });
 
-    function handleSignInChange(e) {
-        setSignInValue ( {...signInValue, [e.target.name]: e.target.value})
-        console.log("VALUE: ", signInValue)
-    }
+    console.log("errors: ", errors)
+    console.log("emailSignUp value: ", getValues("emailSignUp"))
 
-    //отправка данных на сервер
-    let userSignIn = {
-        email: signInValue.email,
-        password: signInValue.password
-    };
-    console.log("USER Sign In: ", userSignIn);
-
-    async function onSubmitSignIn(e) {
-        e.preventDefault();
-        await fetch('https://dev.lectonic.ru/api/auth/login/', {
+//ВХОД
+//отправка данных на сервер
+   async function onSubmitSignIn (dataSignIn, event) {
+        event.preventDefault();
+        fetch('https://dev.lectonic.ru/api/auth/login/', {
             method: 'POST',
             headers: {
                 'Access-Control-Allow-Origin': '*'
             },
-            body: JSON.stringify(userSignIn),
-
+            body: JSON.stringify(dataSignIn),
         })
             .then((response) => {
                 console.log("RESPONSE: ", response);
@@ -48,34 +47,21 @@ function Authorization() {
             })
     }
 
-    //Checkbox не выходить из системы
+//Checkbox не выходить из системы
     const [loggedIn, setLoggedIn] = useState(false);
 
     function handleLoggedIn(){
         setLoggedIn(!loggedIn)
     }
 
-    //РЕГИСТРАЦИЯ
-    //изменение значений в инпутах
-    const [signUpValue, setSignUpValue] = useState({
-        name: "",
-        email: "",
-        password: "",
-        password2: ""
-    });
-
-    function handleSignUpChange(e) {
-        setSignUpValue ( {...signUpValue, [e.target.name]: e.target.value})
-        console.log("VALUE: ", signUpValue)
-    }
 
 
-
-    //отправка данных на сервер
-    //ввод e-mail (временно так, пока не готово api с отправкой письма на почту)
-    function onSubmitSignUpEmail(e) {
-        e.preventDefault();
-        window.sessionStorage.setItem("email", signUpValue.email);
+//РЕГИСТРАЦИЯ
+//отправка данных на сервер
+//ввод e-mail (временно так, пока не готово api с отправкой письма на почту)
+    function onSubmitSignUpEmail(event) {
+       event.preventDefault();
+       window.sessionStorage.setItem("email", getValues("emailSignUp"));
         console.log("this session e-mail: ", window.sessionStorage.getItem("email"));
         navigate("/verify_email");
     }
@@ -104,76 +90,79 @@ function Authorization() {
             })
     }*/
 
-    //Checkbox согласие на обработку персональных данных
+//Checkbox согласие на обработку персональных данных
     const [agree, setAgree] = useState(false);
 
     function handleAgree(){
         setAgree(!agree)
     }
 
-    //переключение блоков Вход и Регистрация
+//переключение блоков Вход и Регистрация
     const [signInShown, setSignInShown] = useState(true);
     const [signUpShown, setSignUpShown] = useState(false);
 
-    function signInHandler() {
+    function handleSignIn() {
         setSignInShown(true);
         setSignUpShown(false);
     }
 
-    function signUpHandler() {
+    function handleSignUp() {
         setSignInShown(false);
         setSignUpShown(true);
     }
 
-    //Показать / скрыть пароль
-    const [hidden, setHidden] = useState(true);
+//Показать / скрыть пароль на входе
+    const [hiddenSignIn, setHiddenSignIn] = useState(true);
 
-    function handleHidden(e) {
-        e.target = setHidden(!hidden)
+    function handleHiddenSignIn() {
+       setHiddenSignIn(!hiddenSignIn)
     }
 
 
     return (
         <div className="auth">
             <div className="auth__header">
-                <p className="auth__header-signIn"
-                   onClick={signInHandler}
-                   style={{color: signInShown ? "#0025FF" : "#B4B4B4"}}>Вход</p>
+                <h2 onClick={handleSignIn}
+                    style={{color: signInShown ? "var(--main-blue)" : "var(--add-darkGrey)"}}>Вход</h2>
 
-                <p className="auth__header-signUp"
-                   onClick={signUpHandler}
-                   style={{color: signUpShown ? "#0025FF" : "#B4B4B4"}}>Регистрация</p>
+                <h2 onClick={handleSignUp}
+                    style={{color: signUpShown ? "var(--main-blue)" : "var(--add-darkGrey)"}}>Регистрация</h2>
             </div>
 
             {/* Блок Вход*/}
             <div style={{display: signInShown ? "block" : "none"}}>
                 <div className="auth__text">Войдите в свой аккаунт</div>
                 <form className="auth__form">
-                    <input name="email"
-                           type="email"
-                           className="auth__form__input"
-                           placeholder="E-mail"
-                           value={signInValue.email}
-                           onChange={handleSignInChange}
-                    />
                     <div className="auth__form__input-wrapper">
-                    <input name="password"
-                           type={ hidden ? "password" : "text" }
-                           className="auth__form__input"
+                        <input className="form__input"
+                               placeholder="E-mail"
+                               style={{borderBottom: errors.email ? "1px solid var(--add-pink)" : ""}}
+                               {...register("email", {
+                                   required: true
+                               })} />
+                        {errors.email && <div className="form__input-error">Заполните поле</div>}
+                    </div>
+
+                    <div className="auth__form__input-wrapper">
+                    <input type={ hiddenSignIn ? "password" : "text" }
+                           className="form__input"
                            placeholder="Пароль"
-                           value={signInValue.password}
-                           onChange={handleSignInChange}
-                    />
+                           style={{borderBottom: errors.password ? "1px solid var(--add-pink)" : ""}}
+                           {...register("password", {
+                               required: true
+                           })} />
+                    {errors.password && <div className="form__input-error">Заполните поле</div>}
+
                     <img
                         className="password-icon"
-                        src={ hidden ? eyeClose : eyeOpen}
+                        src={ hiddenSignIn ? eyeClose : eyeOpen}
                         alt="показать/скрыть"
-                        onClick={handleHidden}
-                    />
+                        onClick={handleHiddenSignIn} />
                     </div>
+
                     <div className="auth__form__password-forgotten">Забыли пароль?</div>
 
-                    <div className="auth__form__checkbox signIn">
+                    <div className="auth__form__checkbox-wrapper signIn">
                         <input className="auth__form__checkbox-switch"
                                id="checkbox"
                                type="checkbox"
@@ -185,30 +174,37 @@ function Authorization() {
 
                     <button className="btn auth__form__btn"
                             type="submit"
-                            onClick={onSubmitSignIn}>Войти</button>
+                            disabled={errors.email || errors.password}
+                            onClick={handleSubmit(onSubmitSignIn)}>Войти</button>
                 </form>
 
                 <div className="auth__socials">
                     <span>или</span>
-                    <button className="btn auth__socials__btn-google"><img src={require("../img/google-icon.svg").default}/>Войти через Google</button>
-                    <button className="btn auth__socials__btn-fb"><img src={require("../img/fb-icon.svg").default}/>Войти через Facebook</button>
-                    <button className="btn auth__socials__btn-vk"><img src={require("../img/vk-icon.svg").default}/>Войти через VK</button>
-                    <p>Ещё нет аккаунта? <span onClick={signUpHandler}>Зарегистрироваться</span></p>
+                    <button className="auth__socials__btn-google"><img src={require("../img/google-icon.svg").default}/>Войти через Google</button>
+                    <button className="auth__socials__btn-fb"><img src={require("../img/fb-icon.svg").default}/>Войти через Facebook</button>
+                    <button className="auth__socials__btn-vk"><img src={require("../img/vk-icon.svg").default}/>Войти через VK</button>
                 </div>
+
+                <div className="auth__bottom-text">Ещё нет аккаунта? <h5 onClick={handleSignUp}>Зарегистрироваться</h5></div>
             </div>
 
             {/* Блок Регистрация*/}
             <div style={{display: signUpShown ? "block" : "none"}}>
                 <div className="auth__text">Для регистрации введите Ваш e-mail</div>
                 <form className="auth__form">
-                    <input name="email"
-                           type="email"
-                           className="auth__form__input signUpEmail"
+                    <div className="auth__form__input-wrapper">
+                    <input className="form__input signUpEmail"
                            placeholder="E-mail"
-                           value={signUpValue.email}
-                           onChange={handleSignUpChange}
-                    />
-                    <div className="auth__form__checkbox">
+                           style={{borderBottom: errors.emailSignUp ? "1px solid var(--add-pink)" : ""}}
+                           {...register("emailSignUp", {
+                               required: true,
+                               pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                           })} />
+                    {errors.emailSignUp?.type == "required" && <div className="form__input-error">Заполните поле</div>}
+                    {errors.emailSignUp?.type == "pattern" && <div className="form__input-error">Некорректная почта</div>}
+                    </div>
+
+                    <div className="auth__form__checkbox-wrapper">
                         <input className="auth__form__checkbox-switch"
                                id="checkbox"
                                type="checkbox"
@@ -219,50 +215,9 @@ function Authorization() {
                     </div>
                     <button className="btn auth__form__btn signUp"
                                 type="submit"
-                                disabled={!agree}
+                                disabled={!agree || errors.emailSignUp}
                                 onClick={onSubmitSignUpEmail}>Зарегистрироваться</button>
                 </form>
-
-
-                {/*
-                <div className="auth__text">Придумайте пароль</div>
-                <form className="auth__form">
-                    <div className="auth__form__input-wrapper">
-                        <input name="password"
-                               type={ hidden ? "password" : "text" }
-                               className="auth__form__input"
-                               placeholder="Пароль"
-                               value={signUpValue.password}
-                               onChange={handleSignUpChange}
-                        />
-                        <img
-                            className="password-icon"
-                            src={ hidden ? eyeClose : eyeOpen}
-                            alt="показать/скрыть"
-                            onClick={handleHidden}
-                        />
-                    </div>
-
-                    <div className="auth__form__input-wrapper">
-                        <input name="password2"
-                               type={ hidden ? "password" : "text" }
-                               className="auth__form__input password2"
-                               placeholder="Повторите пароль"
-                               value={signUpValue.password2}
-                               onChange={handleSignUpChange}
-                               style={{borderBottom: ((signUpValue.password && signUpValue.password2) && (signUpValue.password !== signUpValue.password2)) ? "1px solid #FF00C1" : "1px solid #000000"}}
-                        />
-                        <img
-                            className="password-icon"
-                            src={ hidden ? eyeClose : eyeOpen}
-                            alt="показать/скрыть"
-                            onClick={handleHidden}
-                        />
-                    </div>
-                    {(signUpValue.password && signUpValue.password2) && (signUpValue.password !== signUpValue.password2) && <div className="auth__form__input-error">Пароли не совпадают</div>}
-                    <button className="btn auth__form__btn signUp"
-                                type="submit">Продолжить</button>
-                </form>  */}
             </div>
         </div>
     )
